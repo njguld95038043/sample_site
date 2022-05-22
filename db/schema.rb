ee7174ba3204f3de85e807e0939146733f7e1ff1
njguld95038043 <t.tamura6060@gmail.com>
2022-05-22 13:19:31 +0000

    [Add]Genreモデル

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_21_060832) do
+ActiveRecord::Schema.define(version: 2022_05_22_131903) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -43,6 +43,12 @@ ActiveRecord::Schema.define(version: 2022_05_21_060832) do
   create_table "deriveries", force: :cascade do |t|
     t.string "postal_code"
     t.string "address"
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "genres", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false

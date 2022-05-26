05073fdabc6b63490eb494dba5e339eec0890968
njguld95038043 <t.tamura6060@gmail.com>
2022-05-26 03:37:34 +0000

    [Add]Orderモデル

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_25_095728) do
+ActiveRecord::Schema.define(version: 2022_05_26_033712) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -79,6 +79,12 @@ ActiveRecord::Schema.define(version: 2022_05_25_095728) do
     t.integer "purchase_price"
     t.integer "quantity"
     t.integer "making_status"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end

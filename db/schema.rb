441f1ea154b0650188cdde1315721e9cb14f3b56
njguld95038043 <t.tamura6060@gmail.com>
2022-05-27 03:46:17 +0000

    [Udate]Orderモデル

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_26_033712) do
+ActiveRecord::Schema.define(version: 2022_05_27_034602) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -87,6 +87,8 @@ ActiveRecord::Schema.define(version: 2022_05_26_033712) do
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "address"
+    t.integer "postage"
   end
 
   create_table "post_images", force: :cascade do |t|

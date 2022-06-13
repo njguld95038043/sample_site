9f34248055c5baf4961574aac0feb1233ce9bc1f
njguld95038043 <t.tamura6060@gmail.com>
2022-06-13 23:41:37 +0000

    [Add]order_names

---
+++ app/controllers/order_names_controller.rb
@@ -0,0 +1,2 @@
+class OrderNamesController < ApplicationController
+end
---
+++ test/controllers/order_names_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class OrderNamesControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
